a3a2389601f913b1114fa872b07234293a61e5c0
lochan patidar <lochanpatidar6@gmail.com>
2026-06-21 05:11:37 -0700

    Create ArrayTraversal.java

---
+++ ArrayTraversal.java
@@ -0,0 +1,11 @@
+public class ArrayTraversal {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements are:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
